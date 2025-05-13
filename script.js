c3dad7f7121ce4d4c3c6dd121e615d5ec944905b
Victoria Morais <victoriamoraisnobre14@gmail.com>
2025-05-13 12:45:04 -0300

    I add zoom on the images

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let = currentCategory = null // Nenhuma categoria selecionada inicialmente
         const img = document.createElement('img');
         img.src = quote.src;
         img.alt = quote.alt;
-        img.classList.add('quote-img');
+        img.classList.add('quote-img', 'zoomable');
         container.appendChild(img);
       });
 
@@ -68,3 +68,8 @@ downloadBtn.style.marginTop = "5px";
     
 })
 
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("zoomable")) {
+    e.target.classList.toggle("zoomed");
+  }
+});
